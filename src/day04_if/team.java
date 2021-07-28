6ce90f035b50a59af19510ce4bd44a1f4b5c5535
jinsung <tjd610@naver.com>
2021-07-28 12:51:57 +0900

    if else

---
+++ src/day04_if/team.java
@@ -0,0 +1,98 @@
+package day04_if;
+
+import java.util.Scanner;
+
+public class team {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+	/*	int n1;
+		System.out.println("입력 : ");
+		n1 = input.nextInt();
+		//System.out.println("입력 : ");
+		if (n1>60) {
+			System.out.println("60보다 큰 수");
+		}else if(n1<40) {
+			System.out.println("40보다 작은 수");
+		}else {
+			System.out.println("40~60 사이의 수");
+		}
+		*/
+		
+		//김성윤
+		/*int n2;
+		System.out.println("점수 : ");
+		n2 = input.nextInt();
+		if (n2>=90) {
+			System.out.println("A");
+		}else if(n2>=80){
+			System.out.println("B");
+		}
+		else if(n2>=70){
+			System.out.println("C");
+		}
+		else if(n2>=60){
+			System.out.println("D");
+		}
+		else {
+			System.out.println("F는 불합격 입니다.");
+		}
+		*/
+		
+		//김준호
+	/*	int n1,n2,n3,sum;
+		System.out.println("세 수 입력 : ");
+		n1=input.nextInt();
+		n2=input.nextInt();
+		n3=input.nextInt();
+		sum = n1+n2+n3;
+		if(sum>=100) {
+			System.out.println("합격입니다.");
+		}
+		else {
+			System.out.println("불합격입니다.");
+		}
+		*/
+		
+		//송지현
+	/*	int n1;
+		System.out.println("나이 : ");
+		n1 = input.nextInt();
+		
+		if(n1 < 8) {
+			System.out.println("취학전 아동입니다. 입장료는 500원 입니다");
+		}else if(n1 <14) {
+			System.out.println("초등학생 입니다. 입장료는 1000원 입니다");
+		}else if(n1 < 20) {
+			System.out.println("중고등학생 입니다. 입장료는 1500원 입니다");
+		}
+		*/
+		
+		//신윤수
+	/*	int n1,n2,sum,n3,n4;
+		double sum1;
+		System.out.println("갯수 : ");
+		n1 = input.nextInt();
+		n2 = input.nextInt();
+		sum=n1+n2;
+		n3 = n1*5000;
+		n4 = n2*6000;
+		sum1=n3+n4;
+		if(sum>=5) {
+			System.out.println(sum1=((int)sum1*0.95));
+		}else if(sum>=10) {
+			System.out.println(sum1=((int)sum1*0.9));
+		}
+		*/
+		
+	
+
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+}
